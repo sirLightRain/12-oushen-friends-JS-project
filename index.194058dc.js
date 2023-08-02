17d6dfa2569287a0bd3494b56514181e6607be72
sirLightRain <34480855+sirLightRain@users.noreply.github.com>
2023-08-02 18:03:07 +0000

    Deploying to gh-pages from @ sirLightRain/12-oushen-friends-JS-project@56ca50b9cd2dc26c2f1e306b710b96b8268e4245 🚀

--- index.194058dc.js
+++ index.194058dc.js
@@ -1,2 +1,2 @@
 console.log("Training to create pull request! Dam yeah!");
-//# sourceMappingURL=index.e779539e.js.map
+//# sourceMappingURL=index.194058dc.js.map
--- index.26c65866.js
+++ index.26c65866.js
@@ -1,2 +1,2 @@
 console.log("Training to create pull request! Dam yeah!");
-//# sourceMappingURL=index.71b0ba05.js.map
+//# sourceMappingURL=index.26c65866.js.map
